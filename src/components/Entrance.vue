f1bbc318e98ac24a6363a271fba3e163e7b7de69
andre-w2 <cibisova267@gmail.com>
2021-08-13 02:43:31 +0500

    delete function getCookie

--- src/components/Entrance.vue
+++ src/components/Entrance.vue
@@ -37,27 +37,19 @@
 </template>
 
 <script>
+import Cookies from '../js.cookie.min.mjs';
+
 export default {
 	data() {
 		return {
 			inputEmail: '',
 			inputPas: '',
-			email: this.getCookie('email'),
-			pas: this.getCookie('password'),
+			email: Cookies.get('email'),
+			pas: Cookies.get('password'),
 			error: ''
 		}
 	},
 	methods: {
-		getCookie(name) {
-		    let nameEQ = name + "=";
-		    let ca = document.cookie.split(';');
-		    for(let i=0;i < ca.length;i++) {
-		        let c = ca[i];
-		        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-		    }
-		    return null;
-		},
 		entrance() {
 			if (this.inputEmail && this.inputPas) {
 				this.error = ''
